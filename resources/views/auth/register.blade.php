0ae66834584e41495d5b803df1042fe5c59dc4fd
Christian Maticorena <hola@eliottack.com>
2020-07-22 19:54:04 -0500

    test firts commit

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -8,23 +8,19 @@
                 <div class="flex justify-center">
                     <img src="{{ asset('images/logo.png') }}" alt="logo lambda">
                 </div>
-
                 <div class="text-center rounded-md bg-red-700 bg-opacity-700 font-bold shadow-2xl text-white h-6">Crear cuenta</div>
             </div>
             <div class="grid-flow-col">
                 <form method="POST" action="{{ route('register') }}">
                     @csrf
-
                     <div class="mb-4 shadow-md">
                         <input id="name" type="text" class="bg-white border border-gray-400 focus:border-yellow-600 rounded-lg py-2 px-4 block w-full appearance-none leading-normal @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required placeholder="Nombre">
-
                         @error('name')
                             <span class="text-red-500 text-xs italic">
                                 <strong>{{ $message }}</strong>
                             </span>
                         @enderror
                     </div>
-
                     <div class="mb-4 shadow-md">
                         <input id="email" type="email" class="bg-white border border-gray-400 focus:border-yellow-600 rounded-lg py-2 px-4 block w-full appearance-none leading-normal @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" placeholder="Email">
 
@@ -34,7 +30,6 @@
                             </span>
                         @enderror
                     </div>
-
                     <div class="mb-4 shadow-md">
                         <input id="password" type="password" class="g-white border border-gray-400 focus:border-yellow-600 rounded-lg py-2 px-4 block w-full appearance-none leading-normal @error('password') is-invalid @enderror" name="password" required placeholder="Contraseña">
 
