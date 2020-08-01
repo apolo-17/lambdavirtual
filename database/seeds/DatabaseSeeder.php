d05df2cb27d31e79e2fcdfd12a24a6d43e698d3c
Apolo-17 <apolinar.morales.a@gmail.com>
2020-08-01 10:13:26 -0500

    Fix identar

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -21,5 +21,6 @@ class DatabaseSeeder extends Seeder
         $this->call(StudentProfileSeeder::class);
         //$this->call(PermissionsTableSeeder::class);
         //Only exist in Master branch
-        //$this->call(Version_1_0_0_Seeder::class);    }
+        //$this->call(Version_1_0_0_Seeder::class);
+    }
 }
