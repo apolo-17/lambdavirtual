fd516e928b2b58498b151ec967b5a9919c4c6331
Apolo-17 <apolinar.morales.a@gmail.com>
2020-08-01 18:14:13 -0500

    Add dashboard button exam

--- resources/views/layouts/dashboard/app.blade.php
+++ resources/views/layouts/dashboard/app.blade.php
@@ -19,6 +19,7 @@
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <link href="{{ asset('css/all.css') }}" rel="stylesheet" >
+    <link href="{{ asset('css/all-admin.css') }}" rel="stylesheet" >
 </head>
 <body>
     <div id="app" class="userswrap">
